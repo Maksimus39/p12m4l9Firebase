11785d67561717aab4efed6c1640546a2fb06425
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-07-01 11:41:13 +0300

    Initial Commit

---
+++ p12m4l9Firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  p12m4l9Firebase
+//
+//  Created by Максим Минаков on 01.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ p12m4l9Firebase/p12m4l9FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  p12m4l9FirebaseApp.swift
+//  p12m4l9Firebase
+//
+//  Created by Максим Минаков on 01.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct p12m4l9FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
